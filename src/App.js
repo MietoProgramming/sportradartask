9907de98d822a7551f75ee313a6f4c16a2328750
MietoProgramming <kamdox7@gmail.com>
2022-03-02 22:49:19 +0100

    Step 3: Add season filter to results table

--- src/App.js
+++ src/App.js
@@ -2,28 +2,65 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { Table, Td } from "./styledComponents/Home";
 import { baseURI, ApiKeyURI } from "./services/settingApi";
-import { schedulesRes } from "./apiResponsesMocks/schedules";
+// import { schedulesRes } from "./apiResponsesMocks/schedules";
+// import { seasonsRes } from "./apiResponsesMocks/seasons";
 
 function App() {
   const [isLoading, setLoading] = useState(false);
-  const [schedulesData, setSchedulesData] = useState({});
+  const [schedulesData, setSchedulesData] = useState([]);
+  const [seasons, setSeasons] = useState([]);
+  const [seasonFilter, setSeasonFilter] = useState("");
+  const [schedulesFiltered, setSchedulesFiltered] = useState([]);
 
   useEffect(() => {
     setLoading(true);
     fetchData();
+    setFilters();
     setLoading(false);
   }, []);
 
+  useEffect(() => {
+    setFilters();
+  }, [seasonFilter]);
+
   const fetchData = async () => {
-    // await fetch(baseURI + 'soccer/trial/v4/en/seasons/sr:season:77453/schedules.json' + ApiKeyURI)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     setData(data);
-    //     console.log(data);
-    //   })
-    //   .catch(error => console.log(error))
-    console.log(schedulesRes.schedules[0]);
-    setSchedulesData(schedulesRes);
+    const schedulesRes = await fetch(
+      baseURI +
+        "soccer/trial/v4/en/seasons/sr:season:77453/schedules.json" +
+        ApiKeyURI
+    )
+      .then((res) => res.json())
+      .catch((error) => console.log(error));
+    const seasonsRes = await fetch(
+      baseURI +
+        "soccer/trial/v4/en/competitions/sr:competition:202/seasons.json" +
+        ApiKeyURI
+    )
+      .then((res) => res.json())
+      .catch((error) => console.log(error));
+    console.log(schedulesRes.schedules);
+    setSchedulesData(schedulesRes.schedules);
+    console.log(seasonsRes.seasons);
+    setSeasons(seasonsRes.seasons);
+  };
+
+  const setFilters = () => {
+    setSchedulesFiltered(
+      seasonFilter
+        ? schedulesData?.filter(
+            (event) =>
+              event.sport_event.sport_event_context.season.id === seasonFilter
+          )
+        : schedulesData
+    );
+    console.log(
+      seasonFilter
+        ? schedulesData?.filter(
+            (event) =>
+              event.sport_event.sport_event_context.season.id === seasonFilter
+          )
+        : schedulesData
+    );
   };
 
   const formatDate = (date) => {
@@ -40,53 +77,71 @@ function App() {
       {isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        <Table>
-          <thead>
-            <tr>
-              <Td colSpan="2">Team Names</Td>
-              <Td>Result</Td>
-              <Td>Match Date</Td>
-              <Td>Half Time Score</Td>
-              <Td>Stadium Name</Td>
-            </tr>
-          </thead>
-          <tbody>
-            {schedulesData?.schedules?.map((match, index) => (
-              <tr key={match.sport_event.id}>
-                <Td
-                  bgColor={setTeamColor(
-                    match.sport_event.competitors[0].id,
-                    match.sport_event_status.winner_id
-                  )}
-                >
-                  {match.sport_event.competitors[0].name}
-                </Td>
-                <Td
-                  bgColor={setTeamColor(
-                    match.sport_event.competitors[1].id,
-                    match.sport_event_status.winner_id
-                  )}
-                >
-                  {match.sport_event.competitors[1].name}
-                </Td>
-                <Td>
-                  {match.sport_event_status.period_scores &&
-                    (match.sport_event.competitors[0].qualifier === "home"
-                      ? `${match.sport_event_status.home_score} : ${match.sport_event_status.away_score}`
-                      : `${match.sport_event_status.away_score} : ${match.sport_event_status.home_score}`)}
-                </Td>
-                <Td>{formatDate(match.sport_event.start_time)}</Td>
-                <Td>
-                  {match.sport_event_status.period_scores &&
-                    (match.sport_event.competitors[0].qualifier === "home"
-                      ? `${match.sport_event_status.period_scores[0].home_score} : ${match.sport_event_status.period_scores[0].away_score}`
-                      : `${match.sport_event_status.period_scores[0]?.away_score} : ${match.sport_event_status.period_scores[0].home_score}`)}
-                </Td>
-                <Td>{match.sport_event.venue.name}</Td>
-              </tr>
+        <>
+          <select
+            name="seasons"
+            id=""
+            onChange={(e) => {
+              console.log(e.target.value);
+              setSeasonFilter(e.target.value);
+            }}
+          >
+            <option value="">-</option>
+            {seasons?.map((season) => (
+              <option key={season.id} value={season.id}>
+                {season.name}
+              </option>
             ))}
-          </tbody>
-        </Table>
+          </select>
+
+          <Table>
+            <thead>
+              <tr>
+                <Td colSpan="2">Team Names</Td>
+                <Td>Result</Td>
+                <Td>Match Date</Td>
+                <Td>Half Time Score</Td>
+                <Td>Stadium Name</Td>
+              </tr>
+            </thead>
+            <tbody>
+              {schedulesFiltered?.map((match, index) => (
+                <tr key={match.sport_event.id}>
+                  <Td
+                    bgColor={setTeamColor(
+                      match.sport_event.competitors[0].id,
+                      match.sport_event_status.winner_id
+                    )}
+                  >
+                    {match.sport_event.competitors[0].name}
+                  </Td>
+                  <Td
+                    bgColor={setTeamColor(
+                      match.sport_event.competitors[1].id,
+                      match.sport_event_status.winner_id
+                    )}
+                  >
+                    {match.sport_event.competitors[1].name}
+                  </Td>
+                  <Td>
+                    {match.sport_event_status.period_scores &&
+                      (match.sport_event.competitors[0].qualifier === "home"
+                        ? `${match.sport_event_status.home_score} : ${match.sport_event_status.away_score}`
+                        : `${match.sport_event_status.away_score} : ${match.sport_event_status.home_score}`)}
+                  </Td>
+                  <Td>{formatDate(match.sport_event.start_time)}</Td>
+                  <Td>
+                    {match.sport_event_status.period_scores &&
+                      (match.sport_event.competitors[0].qualifier === "home"
+                        ? `${match.sport_event_status.period_scores[0].home_score} : ${match.sport_event_status.period_scores[0].away_score}`
+                        : `${match.sport_event_status.period_scores[0]?.away_score} : ${match.sport_event_status.period_scores[0].home_score}`)}
+                  </Td>
+                  <Td>{match.sport_event.venue.name}</Td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
       )}
     </div>
   );
---
+++ src/apiResponsesMocks/seasons.js
@@ -0,0 +1,29 @@
+export const seasonsRes = {
+    "generated_at": "2022-03-02T19:08:28+00:00",
+    "seasons": [
+        {
+            "id": "sr:season:67233",
+            "name": "Ekstraklasa 19\/20",
+            "start_date": "2019-07-19",
+            "end_date": "2020-07-20",
+            "year": "19\/20",
+            "competition_id": "sr:competition:202"
+        },
+        {
+            "id": "sr:season:77453",
+            "name": "Ekstraklasa 20\/21",
+            "start_date": "2020-08-21",
+            "end_date": "2021-05-16",
+            "year": "20\/21",
+            "competition_id": "sr:competition:202"
+        },
+        {
+            "id": "sr:season:84320",
+            "name": "Ekstraklasa 21\/22",
+            "start_date": "2021-07-23",
+            "end_date": "2022-05-21",
+            "year": "21\/22",
+            "competition_id": "sr:competition:202"
+        }
+    ]
+}
